3a66096fdae0d0ce4086af3b0a899faf5d854d31
caohuilin <caohuilin@bytedance.com>
2022-11-01 18:26:58 +0800

    feat: debug

--- bin/run.js
+++ bin/run.js
@@ -8,16 +8,16 @@ const srcPath = pkgInfo['jsnext:source'];
 const distPath = pkgInfo.main;
 const project = path.join(__dirname, '../tsconfig.json');
 let env = 'production';
-if (fs.existsSync(project)) {
-  env = 'development';
-}
+// if (fs.existsSync(project)) {
+//   env = 'development';
+// }
 if (process.env.CODESMITH_ENV) {
   env = process.env.CODESMITH_ENV;
 }
 
-if (env === 'development') {
-  require('ts-node').register({ project });
-}
+// if (env === 'development') {
+//   require('ts-node').register({ project });
+// }
 
 try {
   require(`../${env === 'development' ? srcPath : distPath}`).default();
